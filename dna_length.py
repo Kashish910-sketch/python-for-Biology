955d3618a05ff400329930264a7304eca2ecac10
Kashish910-sketch <shaluharish1998@gmail.com>
2026-04-10 05:04:32 +0530

    Create dna_length.py for DNA length calculation
    Added DNA length calculator

---
+++ dna_length.py
@@ -0,0 +1,9 @@
+# DNA Length Calculator
+# Author = Kashish
+# Description = CALCULATE THE LENGTH OF DNA SEQUENCE
+dna = "ATGCGTACGTTAGC"
+
+length = len(dna)
+
+print("DNA Sequence:", dna)
+print("Length:", length)
